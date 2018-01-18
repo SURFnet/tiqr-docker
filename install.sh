9c51e26f9bcf7fc1541f8c4db5c3bbe539bb82ae
Lineke Kerckhoffs-Willems <lineke@egeniq.com>
2018-01-18 12:20:12 +0100

    Changed install a bit, since there were errors and added missing Dockerfile

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-rm -Rf web/src/simplesamlphp /web/src/simplesamlphp-module-authtiqr web/src/tiqr-server-libphp web/src/php-qr-code web/src/zf1
+rm -Rf web/src/simplesamlphp web/src/simplesamlphp-module-authtiqr web/src/tiqr-server-libphp web/src/php-qr-code web/src/zf1
 git clone git@github.com:simplesamlphp/simplesamlphp.git web/src/simplesamlphp
 git clone git@github.com:SURFnet/simplesamlphp-module-authtiqr.git web/src/simplesamlphp-module-authtiqr
 git clone git@github.com:SURFnet/tiqr-server-libphp.git web/src/tiqr-server-libphp
@@ -7,8 +7,9 @@ git clone git@github.com:intern/PHP-QR-Code.git web/src/php-qr-code
 git clone git@github.com:zendframework/zf1.git web/src/zf1
 
 mkdir -p web/src/simplesamlphp/config && cp -r web/src/simplesamlphp/config-templates/* web/src/simplesamlphp/config/
+mkdir -p web/src/simplesamlphp-module-authtiqr/config && cp -r web/src/simplesamlphp-module-authtiqr/config-templates/* web/src/simplesamlphp-module-authtiqr/config/
 mkdir -p web/src/simplesamlphp/metadata && cp -r web/src/simplesamlphp/metadata-templates/* web/src/simplesamlphp/metadata/
 
-cp web/assets/simplesamlphp/config/config.php web/src/simplesamlphp/config/config.php
-cp web/assets/simplesamlphp/config/module_tiqr.php web/src/simplesamlphp/config/module_tiqr.php
-cp web/assets/simplesamlphp/config/authsources.php web/src/simplesamlphp/config/authsources.php
+cp web/src/simplesamlphp/config/config.php web/assets/simplesamlphp/config/config.php
+cp web/src/simplesamlphp-module-authtiqr/config/module_tiqr.php web/assets/simplesamlphp/config/module_tiqr.php
+cp web/src/simplesamlphp/config/authsources.php web/assets/simplesamlphp/config/authsources.php
