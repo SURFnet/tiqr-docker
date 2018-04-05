1c75b5f13bf216e85739b13951a88daea01e1e89
Sandy Pleyte <sandy@egeniq.com>
2018-04-05 14:45:34 +0200

    Updated docker setup

--- web/assets/entrypoint.sh
+++ web/assets/entrypoint.sh
@@ -1,8 +1,71 @@
 #!/usr/bin/env bash
 
-mkdir -p /var/simplesamlphp/cert && openssl req -x509 -batch -nodes -newkey rsa:2048 -keyout /var/simplesamlphp/cert/saml.pem -out /var/simplesamlphp/cert/saml.crt
 
-cd /var/simplesamlphp
+
+# Remove old directories
+if [ -d "/src/simplesamlphp" ]; then
+  echo "Remove old simplesamlphp directory"
+  rm -Rf /src/simplesamlphp
+fi
+
+if [ -d "/src/simplesamlphp-module-authtiqr" ]; then
+  echo "Remove old simplesamlphp-module-authtiqr directory"
+  rm -Rf /src/simplesamlphp-module-authtiqr
+fi
+
+if [ -d "/src/tiqr-server-libphp" ]; then
+  echo "Remove old tiqr-server-libphp directory"
+  rm -Rf /src/tiqr-server-libphp
+fi
+
+if [ -d "/src/php-qr-code" ]; then
+  echo "Remove old php-qr-code directory"
+  rm -Rf /src/php-qr-code
+fi
+
+if [ -d "/src/zf1" ]; then
+  echo "Remove old zf1 directory"
+  rm -Rf /src/zf1
+fi
+
+# Clone repositories
+echo "Clone required repositories..."
+echo "Clone simplesamlphp ..."
+git clone https://github.com/simplesamlphp/simplesamlphp.git /src/simplesamlphp
+echo "Clone simplesamlphp module authtiqr ..."
+git clone https://github.com/SURFnet/simplesamlphp-module-authtiqr.git /src/simplesamlphp-module-authtiqr
+echo "Clone tiqr server libphp ..."
+git clone https://github.com/SURFnet/tiqr-server-libphp.git /src/tiqr-server-libphp
+echo "Clone PHP QR Code ..."
+git clone https://github.com/intern/PHP-QR-Code.git /src/php-qr-code
+echo "Clone ZendFramework 1 ..."
+git clone https://github.com/zendframework/zf1.git /src/zf1
+
+# Copy default configuration
+echo "Copy default configuration..."
+mkdir -p /src/simplesamlphp/config && cp -r /src/simplesamlphp/config-templates/* /src/simplesamlphp/config/
+mkdir -p /src/simplesamlphp/metadata && cp -r /src/simplesamlphp/metadata-templates/* /src/simplesamlphp/metadata/
+
+# Copy tiqr configuration
+echo "Copy tiqr configuration files..."
+cp /assets/simplesamlphp/config/config.php /src/simplesamlphp/config/config.php
+cp /assets/simplesamlphp/config/module_tiqr.php /src/simplesamlphp/config/module_tiqr.php
+cp /assets/simplesamlphp/config/authsources.php /src/simplesamlphp/config/authsources.php
+
+# Create symlinks
+echo "Create symlinks for all repositories..."
+mkdir -p /src/simplesamlphp/modules
+ln -s /src/simplesamlphp-module-authtiqr /src/simplesamlphp/modules/authTiqr
+ln -s /src/simplesamlphp /var/simplesamlphp
+ln -s /src/tiqr-server-libphp /var/libTiqr
+ln -s /src/php-qr-code /var/php-qr-code
+ln -s /src/zf1 /var/zendframework1
+
+# Generate ssl certificate
+echo "Generate SSL Certificate..."
+mkdir -p /src/simplesamlphp/cert && openssl req -x509 -batch -nodes -newkey rsa:2048 -keyout /src/simplesamlphp/cert/saml.pem -out /src/simplesamlphp/cert/saml.crt
+
+cd /src/simplesamlphp
 if [ ! -d "vendor/composer" ]; then
      echo "Installing dependencies..."
      curl -sS https://getcomposer.org/installer | php
